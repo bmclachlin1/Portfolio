0b71dc4eca9984d95d0293faf31a96ef649a86d4
bmclachlin1 <blake.mclachlin@uleth.ca>
2020-08-28 18:40:05 -0600

    attempt fixing form

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,15 +1,17 @@
 <?php
-    $name = $_POST('name');
-    $from_email = $_POST('email');
-    $subject = $_POST('subject');
-    $message = $_POST('message');
-
-    $to_email = 'blake.mclachlin@icloud.com';
+    if(isset($_POST('submit'))) {
+        $name = $_POST('name');
+        $from_email = $_POST('email');
+        $subject = $_POST('subject');
+        $message = $_POST('message');
     
-    $headers = "From: ".$from_email;
-    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
-
-    mail($to_email, $subject, $txt, $headers);
-
-    header("Location: index.html");
+        $to_email = 'blake.mclachlin@icloud.com';
+        
+        $headers = "From: ".$from_email;
+        $txt = "You have received an e-mail from ".$name.".\n\n".$message;
+    
+        mail($to_email, $subject, $txt, $headers);
+    
+        // header("Location: index.php?mailsend");
+    }
 ?>
