89d5ca02aa25d4a253a32cc5607a4c14e7530eaa
bmclachlin1 <blake.mclachlin@uleth.ca>
2020-08-30 14:42:11 -0600

    pls work

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -1,4 +1,17 @@
 <?php
+
+    function console_log($data, $add_script_tags = false) {
+        $command = 'console.log('. json_encode($data, JSON_HEX_TAG).');';
+        if ($add_script_tags) {
+            $command = '<script>'. $command . '</script>';
+        }
+        echo $command;
+    }
+
+    error_reporting(-1);
+    ini_set('display_errors', 'On');
+    set_error_handler("var_dump");
+
      if(isset($_POST['submit'])) {
         $name = $_POST['name'];
         $from_email = $_POST['email'];
@@ -10,8 +23,12 @@
         $headers = "From: ".$from_email;
         $txt = "You have received an e-mail from ".$name.".\n\n".$message;
     
-        mail($to_email, $subject, $txt, $headers);
-    
-        header("Location: index.html");
+        if(mail($to_email, $subject, $txt, $headers)) {
+            echo "true";
+        } else {
+            echo "false";
+        }
+
+        //header("Location: index.html");
      }
 ?>
